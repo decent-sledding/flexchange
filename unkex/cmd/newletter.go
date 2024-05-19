672283c82495b713784b493fa69bccaa7a156236
bitwunder <ethereum51@gmail.com>
2024-05-19 22:58:06 +0300

    refactor: move subscription service

--- unkex/cmd/newletter.go
+++ unkex/cmd/newletter.go
@@ -12,6 +12,12 @@ import (
 func main() {
 	conf := config.GetConfig()
 
+	err = storage.RunMigrations(connectionString)
+
+	if err != nil {
+		return err
+	}
+
 	cr := cron.New()
 	cr.AddFunc("0 0 0 * * *", main_api.handleSubscriptionNewsletter(&conf))
 	cr.Start()
